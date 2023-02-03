5c1798f0101635b705fdae5eb4a842a88f35980d
toofanjhala <jhalatoofan@gmail.com>
2023-02-03 16:34:23 +0530

    all file till task 4

---
+++ src/components/expenses/expenseform.js
@@ -0,0 +1,15 @@
+function ExpenseForm() {
+    return (
+       
+            <form>
+                <label> EXPENSE TITLE</label>
+                <input type="text"></input>
+                <label> Amount</label>
+                <input type="number"></input>
+                <lable> Choose a Date</lable>
+                <input type="date"></input>
+            </form>
+    )
+}
+
+export default ExpenseForm
